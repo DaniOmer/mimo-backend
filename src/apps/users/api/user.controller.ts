44a29987b52fcd26a3700a939327c36a047a4714
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-11-21 12:57:41 +0100

    Added logging to user controller;

--- src/apps/users/api/user.controller.ts
+++ src/apps/users/api/user.controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import { UserService } from "../domain/user.service";
+import { Logger } from "winston";
+import { LoggerConfig } from "../../../config/logger/logger.config";
 
 export class UserController {
   readonly userService: UserService;
+  private logger: Logger;
 
   constructor() {
     this.userService = new UserService();
+    this.logger = LoggerConfig.get().logger;
   }
 
   async register(req: Request, res: Response): Promise<void> {
@@ -13,8 +17,10 @@ export class UserController {
 
     try {
       const newUser = await this.userService.register(userData);
+      this.logger.info(`User registered: ${newUser.email}`);
       res.status(201).json(newUser);
     } catch (error: any) {
+      this.logger.error(`Error registering user: ${error.message}`);
       res.status(400).json({ message: error.message });
     }
   }
