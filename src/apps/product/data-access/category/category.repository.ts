f54f45b10699a368e1b20e24ff835489d868835d
faez <benfaez07@gmail.com>
2024-11-30 11:04:07 +0100

    feat(data-access): add repository and model implementations for product-related entities
    - Implemented data-access layers for the following models:
      - `Category`: Added schema and repository to manage product categories.
      - `Color`: Added schema and repository for color management.
      - `Size`: Added schema and repository for size definitions.
      - `ProductVariant`: Added schema and repository for managing product variants, including relationships with size, color, and product.
      - `ProductFeature`: Added schema and repository for product features.
      - `ProductImage`: Added schema and repository for handling product images.
      - `ProductHasCategory`: Added schema and repository for managing product-category relationships.
      - `ProductHasProductFeature`: Added schema and repository for product-feature associations.

---
+++ src/apps/product/data-access/category/category.repository.ts
@@ -0,0 +1,17 @@
+import { MongooseRepository } from "../../../../librairies/repositories/mongoose/mongoose.repository";
+import { CategoryModel } from "./category.model";
+import { ICategory } from "./category.interface";
+
+export class CategoryRepository extends MongooseRepository<ICategory> {
+  constructor() {
+    super(CategoryModel);
+  }
+
+  async getAllWithParent(): Promise<ICategory[]> {
+    return this.model.find().populate("parentId").exec();
+  }
+
+  async findByIdWithParent(categoryId: string): Promise<ICategory | null> {
+    return this.model.findById(categoryId).populate("parentId").exec();
+  }
+}
---
+++ src/apps/product/data-access/size/size.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+import { ISize } from "./size.interface";
+
+const SizeSchema = new Schema<ISize>(
+  {
+    name: { type: String, required: true },
+    dimensions: { type: String },
+    volume: { type: Number },
+    weightCapacity: { type: Number },
+    isPopular: { type: Boolean, default: false },
+  },
+  { timestamps: true, collection: "sizes", versionKey: false }
+);
+
+export const SizeModel = model<ISize>("Size", SizeSchema);
