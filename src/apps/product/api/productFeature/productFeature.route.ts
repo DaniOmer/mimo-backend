c623c158b45c5545a395a03504cdd73df8a508ff
faez <benfaez07@gmail.com>
2024-12-24 13:49:03 +0100

    feat(product-feature): Implement CRUD operations for ProductFeature
    - Added routes for creating, reading, updating, and deleting product features.
    - Integrated middleware for validation, authentication, and role-based access control.
    - Included Swagger documentation for all API endpoints.
    - Ensured DTO validation for create and update operations.
    - Added support for retrieving all product features and individual features by ID.

---
+++ src/apps/product/api/productFeature/productFeature.route.ts
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import { ProductFeatureController } from "./productFeature.controller";
+import {
+  validateDtoMiddleware,
+  validateIdMiddleware,
+  authenticateMiddleware,
+  checkRoleMiddleware,
+} from "../../../../librairies/middlewares";
+import { ProductFeatureCreateDTO , ProductFeatureUpdateDTO} from "../../domain";
+
+const productFeatureController = new ProductFeatureController();
+const router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: ProductFeatures
+ *   description: API for managing product features
+ */
+
+router.post(
+  "/",
+  authenticateMiddleware,
+  checkRoleMiddleware(["admin"]),
+  validateDtoMiddleware(ProductFeatureCreateDTO),
+  productFeatureController.create.bind(productFeatureController)
+);
+
+router.get("/",
+    authenticateMiddleware,
+   productFeatureController.getAll.bind(productFeatureController));
+
+router.get(
+  "/:id",
+  authenticateMiddleware,
+  validateIdMiddleware("ProductFeature"),
+  productFeatureController.getById.bind(productFeatureController)
+);
+
+router.put(
+  "/:id",
+  authenticateMiddleware,
+  checkRoleMiddleware(["admin"]),
+  validateIdMiddleware("ProductFeature"),
+  validateDtoMiddleware(ProductFeatureUpdateDTO),
+  productFeatureController.updateById.bind(productFeatureController)
+);
+
+router.delete(
+  "/:id",
+  authenticateMiddleware,
+  checkRoleMiddleware(["admin"]),
+  validateIdMiddleware("ProductFeature"),
+  productFeatureController.deleteById.bind(productFeatureController)
+);
+
+export default router;
