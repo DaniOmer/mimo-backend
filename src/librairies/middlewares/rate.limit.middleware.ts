3bdf74cb098420d32c9f685e0e9715d448413ad4
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-12-30 14:31:16 +0100

    Added changes related to CORS and Rate limit

--- src/librairies/middlewares/rate.limit.middleware.ts
+++ src/librairies/middlewares/rate.limit.middleware.ts
@@ -2,6 +2,6 @@ import { rateLimit } from "express-rate-limit";
 
 export const rateLimiterMiddleware = rateLimit({
   windowMs: 60 * 1000,
-  max: 5,
+  max: 20,
   message: "Too many requests, please try again later",
 });
