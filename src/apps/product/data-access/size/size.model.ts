3b3753fc2d1ca17cf72b071f229f5e13f4411b4f
faez <benfaez07@gmail.com>
2025-01-05 15:27:20 +0100

    fix(size) add required and default option in model

--- src/apps/product/data-access/size/size.model.ts
+++ src/apps/product/data-access/size/size.model.ts
@@ -5,8 +5,8 @@ const SizeSchema = new Schema<ISize>(
   {
     name: { type: String, required: true },
     dimensions: { type: String, required: true},
-    volume: { type: Number },
-    weightCapacity: { type: Number },
+    volume: { type: Number, required: false, default: null},
+    weightCapacity: { type: Number, required: false, default: null},
     isPopular: { type: Boolean, default: false },
   },
   { timestamps: true, collection: "sizes", versionKey: false }
